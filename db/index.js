a35ce396afe7f0cf0701b519d162ce9413e3c8b8
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-10-21 11:22:34 -0700

    feat(refactor DB layout). Closes #14.

--- db/index.js
+++ db/index.js
@@ -1,62 +1,8 @@
-const { Client } = require("pg");
-const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/shoes-dev',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-});
+// require and re-export all the files in the adapterMethods folder
 
-// database methods
-
-async function getProductById(id) {
-  try {
-    const { rows: [ product ]  } = await client.query(`
-      SELECT *
-      FROM products
-      WHERE id=$1;
-    `, [id]);
-    return product;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function getAllProducts() {
-  try {
-    const { rows: [ products ]  } = await client.query(`
-      SELECT *
-      FROM products;
-    `);
-    return products;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function createProduct({
-  name,
-  description,
-  price,
-  imgURL,
-  inStock,
-  category
-}) {
-  try {
-    const { rows: [ product ] } = await client.query(`
-      INSERT INTO products(name, description, price, "imgURL", "inStock", category) 
-      VALUES($1, $2, $3, $4, $5, $6)
-      RETURNING *;
-    `, [name, description, price, imgURL, inStock, category]);
-
-    return product;
-  } catch (error) {
-    throw error;
-  }
-}
-
-// export
 module.exports = {
-  client,
-  getProductById,
-  getAllProducts,
-  createProduct
-  // db methods
+  ...require("./adpaterMethods/products"),
+  ...require("./adpaterMethods/users"),
+  ...require("./adpaterMethods/order_products"),
+  ...require("./adpaterMethods/orders")
 }
