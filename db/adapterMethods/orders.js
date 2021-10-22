80f674576f5f04e04a990a7a3cf091fe0d3ff600
darrian77 <jason.lammers@outlook.com>
2021-10-22 01:11:27 -0700

    createUser, createOrder, seed data for users and orders, minor orders table change, fixed folder name typo

---
+++ db/adapterMethods/orders.js
@@ -0,0 +1,18 @@
+const client = require("../client");
+
+const createOrder = async ({status, userId}) => {
+    try {
+        const {rows: [order] } = await client.query(`
+            INSERT INTO orders (status, "userId")
+            VALUES ($1, $2)
+            RETURNING *;
+        `, [status, userId]);
+        return order;        
+    } catch (error) {
+        console.error(error)
+    };
+};
+
+module.exports = {
+    createOrder
+}
