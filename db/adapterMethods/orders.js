fb419fdc0d46db98d56e73334feb71754bfa64c1
darrian77 <jason.lammers@outlook.com>
2021-10-22 21:09:28 -0700

    getOrderById

--- db/adapterMethods/orders.js
+++ db/adapterMethods/orders.js
@@ -2,7 +2,7 @@ const client = require("../client");
 
 const createOrder = async ({status, userId}) => {
     try {
-        const {rows: [order] } = await client.query(`
+        const { rows: [order] } = await client.query(`
             INSERT INTO orders (status, "userId")
             VALUES ($1, $2)
             RETURNING *;
@@ -10,6 +10,20 @@ const createOrder = async ({status, userId}) => {
         return order;        
     } catch (error) {
         console.error(error)
+    };
+};
+
+const getOrderById(id) = async => {
+    try {
+        const { rows: [order]} = await client.query (`
+            SELECT * FROM orders
+            WHERE id = $1;
+        `,[id]);
+        return order;
+    } catch (error) {
+        catch (error) {
+            console.errorb(error);
+        };
     };
 };
 
