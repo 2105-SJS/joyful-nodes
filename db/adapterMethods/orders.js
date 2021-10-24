633eb62d54738e573b06f5cdf935d4dfc48ef0ec
darrian77 <jason.lammers@outlook.com>
2021-10-24 01:34:23 -0700

    changed client require in orders adapters

--- db/adapterMethods/orders.js
+++ db/adapterMethods/orders.js
@@ -1,4 +1,4 @@
-const client = require("../client");
+const { client } = require("../client");
 const { getOrderProductsByOrder } = require ('./order_products');
 const { getProductById } = require('./products');
 
