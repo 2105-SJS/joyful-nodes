01d5f651fdb798d30ed69887ca6a11590582f062
darrian77 <jason.lammers@outlook.com>
2021-10-24 22:16:16 -0700

    some cleanup @ ./index.js

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const { PORT = 3000 } = process.env;
 const server = express();
 const morgan = require("morgan");
 const { client } = require('./db');
-
-
+const path = require('path');
 
 server.use(express.json());
-
 server.use(morgan("dev"));
-
-const path = require('path');
 server.use(express.static(path.join(__dirname, 'build')));
 
 server.use('/api', require('./routes'));
@@ -21,11 +17,8 @@ server.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 });
 
-
-
 server.listen(PORT, async () => {
   console.log(`Server is running on ${PORT}!`);
-
   try {
     await client.connect();
     console.log('Database is open for business!');
