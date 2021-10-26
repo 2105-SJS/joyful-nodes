f4f8b00750c529456e1bb222933456e3e30161d4
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-10-25 20:56:57 -0700

    updated func

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
 import {
-  getSomething
 } from '../api';
 
 const App = () => {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    getSomething()
-      .then(response => {
-        setMessage(response.message);
-      })
-      .catch(error => {
-        setMessage(error.message);
-      });
-  });
+  // useEffect(() => {
+  //   getSomething()
+  //     .then(response => {
+  //       setMessage(response.message);
+  //     })
+  //     .catch(error => {
+  //       setMessage(error.message);
+  //     });
+  // });
 
   return (
     <div className="App">
