d912bfc0ceec32d2a755b0fabedb5ba66ec00f03
Juan Soto <sotoj88515@gmail.com>
2021-10-30 19:44:33 -0700

    finished adapters/wrote products tests

--- db/adapterMethods/products.js
+++ db/adapterMethods/products.js
@@ -52,7 +52,7 @@ const destroyProduct = async (id) => {
     try {
         const {rows: [product] } = await client.query(`
             DELETE FROM products
-            WHERE id = $1
+            WHERE id=$1
             RETURNING *;
         `, [id]);
         await client.query(`
