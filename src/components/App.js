f5d5a79c97bef2a5eb8bf843040f46cdfab05e4d
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-11-01 20:38:25 -0700

    feat(issue#46): created Products route

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from "react-router-dom";
+import { callApi } from '../util';
 import {
   Home,
   Products,
-  SingleOrder
+  SingleOrder,
 } from './';
 
 const App = () => {
+  // const [token, setToken] = useState("");
+  const [products, setProducts] = useState([]);
+
+  const allProducts = async () => {
+    try {
+      const response = await callApi({
+        url: "/products"
+      });
+      console.log(response)
+      if (response) {
+        setProducts(response);
+      }
+
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    allProducts();
+  }, []);
 
   return (
     <Switch>
       <Route exact path="/">
         <Home />
       </Route>
-      <Route exact path = '/orders/:orderId'>
+      <Route exact path='/orders/:orderId'>
         <SingleOrder />
       </Route>
-      <Route path="/products">
-        <Products />
+      <Route exact path="/products">
+        <Products products={products} />
       </Route>
     </Switch>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Home} from "./Home";
 export { default as Products } from './Products';
 export { default as SingleOrder } from './SingleOrder';
+export {default as SingleProduct} from "./SingleProduct"
