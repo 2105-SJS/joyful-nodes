cf7139a15a051ba53e5dfe39cd653088ca3e0305
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-11-02 00:41:20 -0700

    feat(issue#46): added singleProduct route. Closes #46

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,11 +1,32 @@
-import React from "react";
-// import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { callApi } from "../util";
 
-const SingleProduct = ({ product }) => {
+const SingleProduct = () => {
+    const { productId } = useParams();
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        const singleProduct = async () => {
+            try {
+                const response = await callApi({
+                    url: `/products/${productId}`
+                })
+                console.log(response);
+                if (response) {
+                    setProduct(response);
+                }
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+        singleProduct();
+    }, [productId]);
 
     return (
         <>
-            <h4>{product.name}</h4>
+            <h2>{product.name}</h2>
             <p>{product.category}</p>
             <p>{product.description}</p>
             <p>{product.price}</p>
--- src/util/index.js
+++ src/util/index.js
@@ -9,7 +9,7 @@ export const callApi = async ({ url, method, token, body }) => {
         };
         if (token) options.headers["Authorization"] = `Bearer ${token}`;
 
-        const response = await fetch(`api${url}`, options);
+        const response = await fetch(`/api/${url}`, options);
         const result = await response.json();
         if (result.error) {
             throw (result.error);
