e6dfe049ee7563745fb3d12e61bb26116722361c
darrian77 <jason.lammers@outlook.com>
2021-11-13 12:59:42 -0800

    updated order quantity in cart

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -4,9 +4,7 @@ import { callApi } from '../util';
 
 const CartProduct = ({ cart, getCart, product, token, children }) => {
     const history = useHistory();
-    const [quantity, setQuantity] = useState(1);
-    
-
+    const [quantity, setQuantity] = useState(Number);
     const handleRemoveFromCart = async () => {
         try {
             const orderProd = await callApi ({ url: `orders/${cart.id}/${product.id}`, token });
@@ -20,15 +18,32 @@ const CartProduct = ({ cart, getCart, product, token, children }) => {
         };
     };
 
+    const updateQuantity = async () => {
+        try {
+            const orderProd = await callApi ({ url: `orders/${cart.id}/${product.id}`, token });
+            const response = await callApi ({ url: `order_products/${orderProd.id}`, method: 'PATCH', body: { quantity }, token})
+            if (response) {
+                await getCart();
+                history.push('/cart')
+            };            
+        } catch (error) {
+            console.error (error);
+        };
+    };
+
     useEffect(() => {
         setQuantity(product.quantity)
-      }, [quantity]);
+      }, []);
 
     return (
         <div className='cart-product'>
             <img className='prodIMG' src={product.imgURL} alt={product.name} width='48' height='48'/>                
             <h3>{product.name}</h3>
-            <p><b>Price:</b> ${product.price}</p><div>Quantity: <input className='cart-quant' type='number' onChange={(event) => setQuantity(event.target.value)} min='1' placeholder={quantity} /></div>
+            <p><b>Price:</b> ${product.price}</p>
+            <div>Quantity: 
+                <input className='cart-quant' type='number' onChange={(event) => setQuantity(event.target.value)} min='1' placeholder={quantity} />
+                <button onClick={updateQuantity}>Update Quantity</button>
+            </div>
             <button onClick={handleRemoveFromCart}>Remove</button>
         </div>
     )
